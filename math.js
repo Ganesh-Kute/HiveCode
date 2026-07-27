30dddf87f217b8a4c2092bba33de071993a4e588
Ganesh Shivlal Kute <101732457+Ganesh-Kute@users.noreply.github.com>
2026-07-27 10:58:10 +0530

    add processCart using calculateTotal

--- math.js
+++ math.js
@@ -1,3 +1,6 @@
-export function computeTotal(price, tax) {
+export function calculateTotal(price, tax) {
   return price + (price * tax);
+}
+export function processCart(cart) {
+  return calculateTotal(cart.price, 0.2);
 }
